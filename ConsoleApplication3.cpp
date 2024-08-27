23f1248aa7763b000b2d7ae00d57593a27085a39
vizam <vizam@MARCOS-VIZA>
2024-08-26 19:02:02 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,28 @@
+// ConsoleApplication3.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int numeros[6] = { 10,20,30,40,50,60 };
+    int n[] = { 3,4,5 };
+        char c[] = {'L','U','I','S'};
+        char s[] = { "MONA LISA" };
+        cout << numeros[5] << "\n";
+        cout << n[1] << "\n";
+        cout << c[3] << "\n";
+        cout << sizeof(s) << "\n";
+        cout << sizeof(n) << "\n";
+            cout << sizeof(numeros) << "\n";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
